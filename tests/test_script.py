56a338a3c918d09e0d9e9891440868f6a73a43c6
Chloe-Thangavelu <83045403+Chloe-Thangavelu@users.noreply.github.com>
2026-04-13 13:24:14 -0700

    test: add comprehensive integration test for ICD-O mapper
    Implemented TestScript class using unittest to verify end-to-end data transformation.
    
    Added test_output_content to validate mapping logic against a "golden standard" DataFrame (verifying 8000 and 801-804 code ranges).
    
    Configured robust pathing to support execution from the /tests directory relative to project root.
    
    Enhanced terminal reporting by capturing subprocess logs and sanitizing Unicode output for Windows environments.
    
    Ensured workspace hygiene with setUp and tearDown logic for dummy test files.

--- tests/test_script.py
+++ tests/test_script.py
@@ -31,10 +31,43 @@ class TestScript(unittest.TestCase):
         # run the script
         result = subprocess.run(['python', str(self.script_path)], capture_output=True, text=True)
         
+        # print the test script logs
+        print("\n---- TEST SCRIPT LOG ----")
+        print(result.stdout.replace('\\u2705', 'PASS:')) 
+        print(result.stderr.replace('\\u2705', 'PASS:')) 
+        print("----------------------------")
+
         # check script ran & output file was created
         self.assertEqual(result.returncode, 0, f"Script crashed with error: {result.stderr}")
         self.output_files = list(self.output_dir.glob("test_input_icdomapper_output_*.tsv"))
         self.assertTrue(len(self.output_files) > 0, "No output file was generated in /output")
+
+        # define expected output
+        expected_data = {
+            'code': ['8000/0', '8000/1', '8000/2', '8000/3', '8000/6', '8011/2'],
+            'term': [
+                "Neoplasm, benign", 
+                "Neoplasm, uncertain whether benign or malignant",
+                "unknown", 
+                "Neoplasm, malignant", 
+                "Neoplasm, metastatic", 
+                "unknown"
+            ],
+            'range': ['800', '800', '800', '800', '800', '801-804'],
+            'group': ["Neoplasms, NOS", "Neoplasms, NOS", "Neoplasms, NOS", 
+                "Neoplasms, NOS", "Neoplasms, NOS", "Epithelial neoplasms, NOS"
+            ]
+        }
+        expected_df = pd.DataFrame(expected_data)
+        
+        # read the output file and compare with expected data
+        output_data = max(self.output_files, key=os.path.getctime)
+        output_df = pd.read_csv(output_data, sep='\t')
+
+        # delete self.assertTrue(output_df.equals(expected_df), "the output file contains the expected data")
+        pd.testing.assert_frame_equal(output_df, expected_df, check_dtype=False)
+        print("Test Passed: The output file contains the expected data.")
+
 
     def tearDown(self):
         """clean up the test files."""
@@ -45,4 +78,4 @@ class TestScript(unittest.TestCase):
                         os.remove(out_file)
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(buffer=False)
